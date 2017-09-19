39ade5a0d1b37b73d4576a667ae1c0cff884d7be
Marcus Parmentier and Victor Puentes <victor@>
2017-09-19 15:21:11 -0700

    Add additional js towards functionality with the if else statement.

--- word-puzzle/js/scripts.js
+++ word-puzzle/js/scripts.js
@@ -1,5 +1,7 @@
 //business (backend) logic
 var vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+var outputArray = [];
+//var consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
 //user (frontend) logic
 $(document).ready(function() {
@@ -7,17 +9,23 @@ $(document).ready(function() {
   for (var i = 0; i < quotes.length; i++) {
     var thisSentence = quotes[i];
     var sentenceArray = thisSentence.split("");
-    //["a", "b", "c", " ", "d"];
+    // var character = [!vowels];
+
     vowels.forEach(function(vowel) {
       sentenceArray.forEach(function(character) {
-        console.log("----------------NEW PAIR----------------");
-        console.log("THE VOWEL IS: " +vowel);
-        console.log("THE CHARACTER IS: "+character);
+
+        // var letters = character[i];
         if (vowel === character) {
-          console.log("=========++++++++++++++++++=====THEY MATCH!!!=======++++++++++++++++++=======");
+          //console.log("=========++++++++++++++++++=====THEY MATCH!!!=======++++++++++++++++++=======");
+          // $(character).replace(function() {
+          outputArray.push("-");
+        }
+        else (vowel != character) {
+          outputArray.push(character);
         }
       })
     })
+
     // console.log(sentenceArray);
 
   }
@@ -25,3 +33,8 @@ $(document).ready(function() {
   // $("button").click(function() {
    //*$("#w1").replace();
 });
+
+//
+// console.log("----------------NEW PAIR----------------");
+// console.log("THE VOWEL IS: " +vowel);
+// console.log("THE CHARACTER IS: "+character);
